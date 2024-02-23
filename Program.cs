24de4e6b142c1c39ac8fe5efb9a8732a3d6782f5
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-02-23 21:47:27 +0100

    Added Broker start Config

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace MqttTest
+﻿using MQTTnet;
+
+using MQTTnet.Server;
+
+namespace MqttTest
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var mqttFactory = new MqttFactory();
+            var mqttServerOptions = new MqttServerOptionsBuilder()
+                .WithDefaultEndpointPort(1883)
+                .Build();
+
+            var mqttServer = mqttFactory.CreateMqttServer(mqttServerOptions);
+            
+            await mqttServer.StartAsync();
+            Console.WriteLine("MQTT Broker started.");
+
+            var cancellationToken = new CancellationTokenSource();
+
+
         }
     }
 }
